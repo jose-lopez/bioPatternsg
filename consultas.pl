6e273b0c8ca8fe3548f9ce26c8dd3b16ab88037c
yacson <yacson@T430>
2018-02-06 23:01:26 -0400

    ajuste

--- consultas.pl
+++ consultas.pl
@@ -1,16 +1,15 @@
+:-style_check(-discontiguous).
 :-[mineria/ontologiaMESH].
 :-[mineria/objetosMinados].
 :-[mineria/well_know_rules].
+:-[baseC].
 
 %************************************************************************
 % Dado un ligando, ¿Cuál (les) receptores lo reconocen y se le enlazan?.
+buscar_receptores(L):-ligand(L),base(C),buscar_en_lista(event(L,E,R),C),receptor(R),write(L),write(" "),write(E),write(" "),write(R),nl,fail.
 
-buscar_receptores(L):-ligandos(O,Xs),buscar_en_lista(L,Xs),receptor_ontologia(O),write(L),write('->'),write(O),nl,fail.
-
-%buscar en ontologia MESH que el objeto sea un receptor
-
-receptor_ontologia(X):-is_a(X,'Receptors'),!.
-receptor_ontologia(X):-is_a(X,Y),receptor_ontologia(Y),!.
+%---------------------------------
+% Dado un complejo (o un grupo de ellos) ¿cuáles tienen rol inhibitorio o estimulatorio?.
 
 %**************************************************************************
 
@@ -29,3 +28,15 @@ buscar_en_lista(L,[L|_]).
 buscar_en_lista(L,[_|Ys]):-buscar_en_lista(L,Ys).
 
 %*************************************************************************
+
+listar_eventos(Obj1,Obj2):-base(C),buscar_en_lista(event(Obj1,E,Obj2),C).
+
+%ayuda con patrones
+
+inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,['bind','activate']),ligand(A),receptor(B).
+
+final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A).
+
+intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B).
+
+%buscar_en_lista(E,['bind','activate','regulate','phosphorylate'])
