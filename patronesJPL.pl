39022a21a69a3442dbc50cf4f68540e51f042dbe
yacson <yacson@T430>
2018-05-19 17:26:00 -0400

    ajuste patrones

--- patronesJPL.pl
+++ patronesJPL.pl
@@ -3,13 +3,18 @@
 :-[baseC].
 %:-[baseGPR].
 
-inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,inhibit]),ligand(A),receptor(B).
+inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind]),ligand(A),receptor(B).
 
-final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[bind,activate,inhibit]).
+final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[bind,activate,inhibit,regulate]).
 
-intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[phosphorylate,activate,inhibit]).
+intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[require,interact,associate,phosphorylate,recruit,recognize,participate,activate]).
 
 buscar_en_lista(L,[L|_]).
 buscar_en_lista(L,[_|Ys]):-buscar_en_lista(L,Ys).
+
+ligand('').
+receptor('').
+transcription_factor('').
+protein('').
 
 %[trimerize,require,heterodimerize,interact,associate,phosphorylate,recruit,dimerize,recognize,participate,activate,inhibit]
