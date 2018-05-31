f1ce89ef006d21a056ac50546a2630932d6b7044
yacson <yacson@T430>
2018-05-30 22:03:24 -0400

    ajustes

--- patronesJPL.pl
+++ patronesJPL.pl
@@ -3,7 +3,7 @@
 :-[baseC].
 %:-[baseGPR].
 
-inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind]),ligand(A),receptor(B).
+inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind]),(ligand(A);receptor(A)),receptor(B).
 
 final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[bind,activate,inhibit,regulate]).
 
@@ -13,7 +13,7 @@ eventoEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E
 
 %Patrones con restricciones de objetos
 
-inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind]),ligand(A),receptor(B).
+inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind]),(ligand(A);receptor(A)),receptor(B).
 
 final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[bind,activate,inhibit,regulate]).
 
@@ -21,10 +21,7 @@ intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,
 
 eventoEspecial_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[require,interact,associate,phosphorylate,recruit,recognize,participate,activate]).
 
-
 buscar_en_lista(L,[L|_]).
 buscar_en_lista(L,[_|Ys]):-buscar_en_lista(L,Ys).
-
-objeto(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A).
 
 %[trimerize,require,heterodimerize,interact,associate,phosphorylate,recruit,dimerize,recognize,participate,activate,inhibit]
