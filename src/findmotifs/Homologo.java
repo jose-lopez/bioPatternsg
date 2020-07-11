414eb27b1357d1850ab73bb93ac90c8f67f7bb6b
yacson ramirez <yacson.ramirez@gmail.com>
2020-07-11 02:27:50 -0400

    -licencia

--- src/findmotifs/Homologo.java
+++ src/findmotifs/Homologo.java
@@ -1,38 +1,16 @@
-///*
-//    Homologo.java
-//
-//
-//    Copyright (C) 2016.
-//    Jose Lopez (jlopez@unet.edu.ve).
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as
-//    published by the Free Software Foundation, either version 3 of the
-//    License, or (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>
-//
-//*/
-//
-//
-//package findmotifs;
-//
-//
-//import uk.ac.roslin.ensembl.datasourceaware.compara.DAHomologyPairRelationship;
-//
-//public class Homologo {
-//    public DAHomologyPairRelationship homologo;
-//    public String region_promotora;
-//    
-//    public Homologo(DAHomologyPairRelationship homologo, String region_promotora)
-//    {
-//        this.homologo = homologo;
-//        this.region_promotora = region_promotora;
-//    }
-//}
+/* 
+ * bioPatternsg
+ * BioPatternsg is a system that allows the integration and analysis of information related to the modeling of Gene Regulatory Networks (GRN).
+ * Copyright (C) 2020
+ * Jose Lopez (josesmooth@gmail.com), Jacinto Dávila (jacinto.davila@gmail.com), Yacson Ramirez (yacson.ramirez@gmail.com).
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
