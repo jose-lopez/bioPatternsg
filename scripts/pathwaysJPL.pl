79338bfd5426ed0923189a560bb8f03c2fa40fd4
jose-lopez <josesmooth@gmail.com>
2020-09-15 11:43:42 -0400

    The GeneradorBC class was updated to improve regulation events detection.

--- scripts/pathwaysJPL.pl
+++ scripts/pathwaysJPL.pl
@@ -3,7 +3,7 @@
 inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind,interact,recognize]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
 %evento final patron tipo 1, tipo 2  transcription_factor -> objeto_de_cierre
-final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[activate,inactivate,repress,inhibit,regulate,stimulate,bind,interact,reveal]).
+final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(A),buscar_en_lista(E,[activate,inactivate,repress,inhibit,regulate,stimulate,bind,interact,reveal]).
 
 %eventos intermedios en patron tipo 1 mas tipo 2 que unen el envento inicial con el evento final:  proteina->proteina
 intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[require,interact,associate,phosphorylate,recruit,recognize,participate]).
@@ -19,7 +19,7 @@ finalEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,
 
 inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[activate,bind,interact]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
-final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[activate,inactivate,repress,inhibit,regulate,stimulate,bind,interact,reveal]).
+final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(A),buscar_en_lista(E,[activate,inactivate,repress,inhibit,regulate,stimulate,bind,interact,reveal]).
 
 intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[require,interact,associate,phosphorylate,recruit,recognize,participate]).
 
